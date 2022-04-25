d4cb5eae24e1ee54e9ad4e2c960a6d9c65339aad
maciejchwast@student.agh.edu.pl <maciejchwast@student.agh.edu.pl>
2022-04-25 19:38:12 +0200

    code formatting

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ class Point:
         self.y = py
 
     def print(self):
-        return "X = "+str(round(self.x, 2))+", Y = "+str(round(self.y, 2))
+        return "X = " + str(round(self.x, 2)) + ", Y = " + str(round(self.y, 2))
 
 
 class Straight:
 
     def print(self):
-        return "y = "+str(round(self.a,2))+"X + "+str(round(self.b,2))
+        return "y = " + str(round(self.a, 2)) + "X + " + str(round(self.b, 2))
 
     def __init__(self, p1: Point, p2: Point):
         self.a = (p1.y - p2.y) / (p1.x - p2.x)
@@ -29,7 +29,7 @@ class Line:
     tail: Point
 
     def print(self):
-        return "head point: "+self.head.print()+" tail point: "+self.tail.print()
+        return "head point: " + self.head.print() + " tail point: " + self.tail.print()
 
     def __init__(self, p1: Point, p2: Point):
         self.head = p1
@@ -46,7 +46,7 @@ class Triangle:
     side2: Straight
     side3: Straight
 
-    def __init__(self,p1: Point,p2: Point, p3:Point):
+    def __init__(self, p1: Point, p2: Point, p3: Point):
         self.tip1 = p1
         self.tip2 = p2
         self.tip3 = p3
@@ -55,15 +55,15 @@ class Triangle:
         self.side3 = Straight(self.tip3, self.tip1)
 
     def draw(self):
-        plt.plot([self.tip1.x,self.tip2.x],[self.tip1.y, self.tip2.y])
-        plt.plot([self.tip2.x,self.tip3.x],[self.tip2.y, self.tip3.y])
-        plt.plot([self.tip3.x,self.tip1.x],[self.tip3.y, self.tip1.y])
+        plt.plot([self.tip1.x, self.tip2.x], [self.tip1.y, self.tip2.y])
+        plt.plot([self.tip2.x, self.tip3.x], [self.tip2.y, self.tip3.y])
+        plt.plot([self.tip3.x, self.tip1.x], [self.tip3.y, self.tip1.y])
         plt.show()
 
     def is_point_inside(self, p: Point):
         plt.plot(p.x, p.y, 'o')
         self.draw()
-        conditions = ["x","x","x"]
+        conditions = ["x", "x", "x"]
         conditions[0] = whichSide(self.side1, p)
         conditions[1] = whichSide(self.side2, p)
         conditions[2] = whichSide(self.side3, p)
@@ -75,9 +75,10 @@ class Triangle:
 
 
 def pointsToLine(p1: Point, p2: Point):
-    a = (p1.y - p2.y)/(p1.x - p2.x)
+    a = (p1.y - p2.y) / (p1.x - p2.x)
     b = p1.y - a * p1.x
-    print("Points "+p1.print()+" and "+p2.print()+" are creating y="+str(round(a, 2))+"x+"+str(round(b, 2)))
+    print(
+        "Points " + p1.print() + " and " + p2.print() + " are creating y=" + str(round(a, 2)) + "x+" + str(round(b, 2)))
     return Line(p1, p2)
 
 
@@ -141,12 +142,12 @@ def pointsToLineTab(p1: Point, p2: Point):
 
 
 def crossingPointCramer(t1, t2):
-    W = t1[0]*t2[1] - t2[0]*t1[1]
-    Wx = -t1[2]*t2[1] - (-t2[2]*t1[1])
-    Wy = t1[0]*(-t2[2]) - t2[0]*(-t1[2])
+    W = t1[0] * t2[1] - t2[0] * t1[1]
+    Wx = -t1[2] * t2[1] - (-t2[2] * t1[1])
+    Wy = t1[0] * (-t2[2]) - t2[0] * (-t1[2])
 
-    x = Wx/W
-    y = Wy/W
+    x = Wx / W
+    y = Wy / W
 
     return Point(x, y)
 
@@ -156,12 +157,14 @@ def crossingPointLines(l1: Line, l2: Line):
     s2 = l2.belongs_to_straight
     return crossingPointCramer(slopeToStandardLinearEquation(s1), slopeToStandardLinearEquation(s2))
 
-def calculateTriangleArea(p1: Point, p2: Point, p3:Point):
+
+def calculateTriangleArea(p1: Point, p2: Point, p3: Point):
     tmp1 = p2.x - p1.x
     tmp2 = p3.y - p1.y
     tmp3 = p3.x - p1.x
     tmp4 = p2.y - p1.y
-    return 0.5 * ((tmp1*tmp2) - (tmp3*tmp4))
+    return 0.5 * ((tmp1 * tmp2) - (tmp3 * tmp4))
+
 
 def lab1_and_2():
     print("Random points: ", Pnt1.print(), " ", Pnt2.print(), " ", Pnt3.print())
@@ -180,16 +183,17 @@ def lab1_and_2():
     print("Points 1 2 and 3 are: 1: ", Pnt1.print(), " 2: ", Pnt2.print(), " 3: ", Pnt3.print())
     print("Area of triangle made from points 1 2 and 3 is ", str(round(calculateTriangleArea(Pnt1, Pnt2, Pnt3), 2)))
 
+
 if __name__ == '__main__':
     Pnt1 = randomPoint()
     Pnt2 = randomPoint()
     Pnt3 = randomPoint()
     Pnt4 = randomPoint()
-    #lab1_and_2()
-    #plt.plot([Pnt1.x, Pnt2.x], [Pnt1.y, Pnt2.y])
-    #plt.plot(Pnt3.x, Pnt3.y,'o')
-    #plt.show()
-    #print("The point is "+whichSide(Straight(Pnt1,Pnt2),Pnt3)+" line")
+    # lab1_and_2()
+    # plt.plot([Pnt1.x, Pnt2.x], [Pnt1.y, Pnt2.y])
+    # plt.plot(Pnt3.x, Pnt3.y,'o')
+    # plt.show()
+    # print("The point is "+whichSide(Straight(Pnt1,Pnt2),Pnt3)+" line")
     t1 = Triangle(Pnt1, Pnt2, Pnt3)
     t1.draw()
-    print(t1.is_point_inside(Pnt4))
+    print("Is point inside a triangle? ", t1.is_point_inside(Pnt4))
